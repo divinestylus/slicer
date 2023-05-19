7fced920f7f755b4a8fd5493328ad44e18e71a43
Varma Cephas <varmac231@gmail.com>
2023-05-19 16:23:39 +0000

    add-coloring-to-successful-or-failed-message

--- slicer/DEBIAN/postinst.sh
+++ slicer/DEBIAN/postinst.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env sh
 set -e
 
+# colors for echo message
+RED='\033[0;31m'
+Green='\033[0;32m'
 # update man page database
 echo "Updating man page database..."
 if ! /usr/share/man/man1/mandb -q; then
+# adds color to echo message
+    echo "${RED}==================================="
     echo "Failed to update man page database."
+    echo "${RED}==================================="
     exit 1
 fi
+echo "${Green}==================================="
 echo "Man page database update successful."
+echo "${Green}==================================="
 
 exit 0
